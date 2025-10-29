9d69107f21a5f7b524042b98f1c0264ad94a8525
tobiasengberg <tobias.engberg@exilic.se>
2025-10-29 14:04:32 +0100

    Cleanup script.js, move eventlisteners to modules file

--- vanilla-js/spreadsheet/modules/setup/table-setup.js
+++ vanilla-js/spreadsheet/modules/setup/table-setup.js
@@ -1,22 +1,29 @@
 import {config} from "../data/config.js";
 
-export const setupFullArea = (dimensions) => {
+export const setupTable = () => {
+    setupFullArea();
+    setupColumnsBar();
+    setupRowsBar();
+    setupWorkArea();
+}
+
+export const setupFullArea = () => {
     let toAdd = document.getElementById("setup");
     toAdd.replaceChildren();
-    let workWidth = 100 * dimensions.columns;
-    let workHeight = 30 * dimensions.rows;
+    let workWidth = 100 * config.dimensions.columns;
+    let workHeight = 30 * config.dimensions.rows;
     toAdd.style.width = workWidth + 30 + "px";
     toAdd.style.height = workHeight + 30 + "px";
     toAdd.style.gridTemplateColumns = `30px ${workWidth}px`;
     toAdd.style.gridTemplateRows = `30px ${workHeight}px`;
 }
 
-export const setupColumnsBar = (dimensions) => {
+export const setupColumnsBar = () => {
     let targetArea = document.getElementById("setup");
     let toAdd = document.createElement("div");
     toAdd.setAttribute("id", "columns-bar");
-    toAdd.style.gridTemplateColumns = `repeat(${dimensions.columns}, 100px)`;
-    for(let i = 0; i < dimensions.columns; i++) {
+    toAdd.style.gridTemplateColumns = `repeat(${config.dimensions.columns}, 100px)`;
+    for(let i = 0; i < config.dimensions.columns; i++) {
         let toAddColumn = document.createElement("div");
         toAddColumn.innerText = String.fromCharCode(65 + i);
         toAdd.appendChild(toAddColumn);
@@ -24,12 +31,12 @@ export const setupColumnsBar = (dimensions) => {
     targetArea.appendChild(toAdd);
 }
 
-export const setupRowsBar = (dimensions) => {
+export const setupRowsBar = () => {
     let targetArea = document.getElementById("setup");
     let toAdd = document.createElement("div");
     toAdd.setAttribute("id", "rows-bar");
-    toAdd.style.gridTemplateRows = `repeat(${dimensions.rows}, 30px)`;
-    for(let i = 0; i < dimensions.rows; i++) {
+    toAdd.style.gridTemplateRows = `repeat(${config.dimensions.rows}, 30px)`;
+    for(let i = 0; i < config.dimensions.rows; i++) {
         let toAddRow = document.createElement("div");
         toAddRow.innerText = i + 1 + "";
         toAdd.appendChild(toAddRow);
@@ -59,4 +66,12 @@ export const setupWorkArea = () => {
         toAdd.appendChild(toAddRow);
     }
     targetArea.appendChild(toAdd);
+}
+
+const doesRowContain = () => {
+    return { message: 412 };
+}
+
+const doesColumnContain = () => {
+    return { message: 411 };
 }
--- vanilla-js/spreadsheet/script.js
+++ vanilla-js/spreadsheet/script.js
@@ -1,127 +1,34 @@
 import {getSelectRectangle} from "./modules/graphics.js";
 import {parseExpression} from "./modules/expression-parsing.js";
-import {setupColumnsBar, setupFullArea, setupRowsBar, setupWorkArea} from "./modules/setup/table-setup.js";
+import {setupTable} from "./modules/setup/table-setup.js";
 import { messages } from "./modules/messages.js";
 import { tableCommands } from "./modules/commands/table-commands.js";
 import {setupContent} from "./modules/setup/content-setup.js";
 import {config} from "./modules/data/config.js";
+import {loadEventListeners} from "./modules/eventlisteners/eventlisteners.js";
+
+window.addEventListener("load", () => {
+    let contentHistory = localStorage.getItem("content");
+    if(contentHistory) {
+        config.content = JSON.parse(contentHistory);
+    }
+    let dimensionsHistory = localStorage.getItem("dimensions");
+    if(dimensionsHistory) {
+        config.dimensions = JSON.parse(dimensionsHistory);
+    }
+    updateSheet();
+    loadEventListeners();
+});
 
 
 const updateSheet = () => {
-  setupFullArea(config.dimensions);
-  setupColumnsBar(config.dimensions);
-  setupRowsBar(config.dimensions);
-  setupWorkArea();
-  setupContent();
-  setInMotion()
+    localStorage.setItem("content", JSON.stringify(config.content));
+    localStorage.setItem("dimensions", JSON.stringify(config.dimensions));
+    setupTable();
+    setupContent();
 }
 
 
 
-document.getElementById("menu").addEventListener("click", (e) => {
-    let outcome = tableCommands[e.target.id]();
-    let canRemove = doesRowContain();
-    console.log(messages[canRemove.message]);
-    updateSheet();
-});
 
 
-
-const doesRowContain = () => {
-    return { message: 412 };
-}
-
-const doesColumnContain = () => {
-    return { message: 411 };
-}
-
-const setInMotion = () => {
-  localStorage.setItem("content", JSON.stringify(config.content));
-  localStorage.setItem("dimensions", JSON.stringify(config.dimensions));
-
-  document.getElementById("workArea").addEventListener("dblclick", (e) => {
-      console.log(e);
-    let target = document.getElementById(e.target.id);
-    if(!target) return;
-    let newKey = e.target.id;
-    target.innerHTML = `<input type="text" value=${target.innerText}>`;
-    target.firstChild.focus();
-    target.firstChild.addEventListener("blur", (e) => {
-      if(e.target.value === "") {
-        if (newKey in config.content) {
-          delete config.content[newKey];
-        }
-        target.removeChild(target.firstChild);
-      } else {
-          config.content[newKey] = e.target.value;
-        setUpWorkArea();
-      }
-    })
-  });
-
-    document.getElementById("workArea").addEventListener("mousedown", (e) => {
-        console.log(e);
-        if (e.target.nodeName === "circle") {
-            console.log("circle");
-        }
-    });
-
-  //Select one or more boxes
-  document.getElementById("workArea").addEventListener("click", (e) => {
-    let selectedElement = document.getElementById(e.target.id);
-    if(!selectedElement) return;
-    if(e.getModifierState("Meta")){
-      if(config.selection.includes(e.target.id)) {
-          config.selection.splice(config.selection.indexOf(e.target.id), 1);
-        selectedElement.style.backgroundColor = "lightblue";
-        return;
-      }
-        config.selection.push(e.target.id);
-      selectedElement.style.backgroundColor = "lightpink";
-      if(document.getElementById("selectRectangle")) {
-        document.getElementById("selectRectangle").remove();
-      }
-    } else {
-        config.selection.forEach((element) => {
-        document.getElementById(element).style.backgroundColor = "lightblue";
-      });
-        config.selection.length = 0;
-        config.selection.push(e.target.id);
-      selectedElement.style.backgroundColor = "lightpink";
-
-      if(!document.getElementById("selectRectangle")) {
-        let selectRectangle = getSelectRectangle();
-        selectRectangle.setAttribute("id", "selectRectangle");
-        document.getElementById("workArea").appendChild(selectRectangle);
-      }
-      let selectRectangle = document.getElementById("selectRectangle");
-      selectRectangle.setAttribute("width", e.target.offsetWidth + "px");
-      selectRectangle.setAttribute("height", e.target.offsetHeight + "px");
-      selectRectangle.style.top = e.target.offsetTop + "px";
-      selectRectangle.style.left = e.target.offsetLeft + "px";
-    }
-
-      document.addEventListener("keydown", (e) => {
-          console.log(e.key);
-          if(e.key === "Escape" && config.selection.length > 0) {
-              config.selection.forEach((element) => {
-                  document.getElementById(element).style.backgroundColor = "lightblue";
-              });
-              config.selection.length = 0;
-          }
-      })
-      if(config.selection.length == 0) document.removeEventListener("keydown", (e) => {});
-  })
-};
-
-window.addEventListener("load", () => {
-  let contentHistory = localStorage.getItem("content");
-  if(contentHistory) {
-      config.content = JSON.parse(contentHistory);
-  }
-  let dimensionsHistory = localStorage.getItem("dimensions");
-  if(dimensionsHistory) {
-      config.dimensions = JSON.parse(dimensionsHistory);
-  }
-  updateSheet();
-});
