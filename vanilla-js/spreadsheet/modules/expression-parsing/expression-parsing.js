deb4ccc9533edb93dd69d516332ffe3159a6fd49
tobiasengberg <tobias.engberg@exilic.se>
2025-11-03 21:26:34 +0100

    Add simplistic referencing

--- vanilla-js/spreadsheet/modules/expression-parsing/expression-parsing.js
+++ vanilla-js/spreadsheet/modules/expression-parsing/expression-parsing.js
@@ -1,3 +1,5 @@
+import {config} from "../data/config.js";
+
 export const parseExpression = (expression) => {
     if(/^[0-9]*$/.test(expression)) return [expression, "number"];
     if(expression.substring(0,1) === '=') return handleFormula(expression.substring(1));
@@ -23,6 +25,8 @@ const handleFormula = (formula) => {
             default: break;
         }
     }
+    if(formula.substring(0,2) === "D4")
+        return parseExpression(config.content.filter(item => item.id === "4-4")[0].value);
     let result = 0;
     formula.split("+").map((item) => { result += parseInt(item)})
     return [result, "number"]
