ed0acf03577b1e51bcf4aa17ebc265646ff1255f
tobiasengberg <tobias.engberg@exilic.se>
2025-11-05 20:19:32 +0100

    Simple line entering (wp)

--- vanilla-js/wordprocessor/wordprocessor.js
+++ vanilla-js/wordprocessor/wordprocessor.js
@@ -8,10 +8,26 @@ const typing = () => {
     let target = document.getElementById("setup");
     let workArea = document.createElement("div");
     workArea.setAttribute("id", "workArea");
-    let text = document.createTextNode("Start typing");
+    let text = document.createElement("span");
+    text.innerHTML = "";
     workArea.appendChild(text);
     target.appendChild(workArea);
     document.addEventListener('keydown', (e) => {
-        text.textContent += e.key;
+        switch (e.key) {
+            case "Enter":
+                text.innerHTML += "<br/>";
+                break;
+            case "Backspace":
+                text.innerHTML = text.innerHTML.slice(0, -1);
+                break;
+            case "Meta":
+            case "Control":
+            case "Shift":
+                break;
+            default:
+                text.innerHTML += e.key;
+                break;
+        }
+
     })
 }
