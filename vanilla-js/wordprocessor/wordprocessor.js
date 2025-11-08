70c238ad96d31bd724bce11968286802917242fe
tobiasengberg <tobias.engberg@exilic.se>
2025-11-08 21:30:28 +0100

    Add new paragraph on enter key

--- vanilla-js/wordprocessor/wordprocessor.js
+++ vanilla-js/wordprocessor/wordprocessor.js
@@ -7,32 +7,43 @@ window.addEventListener('load', () => {
         console.log(stylesheets);
         document.styleSheets[1].cssRules[0].style.setProperty("--left-margin", parseInt(stylesheets.split('p')[0]) + 20 + "px");
     });
+    setUpWorkArea();
     typing();
 
 });
 
-const typing = () => {
+const setUpWorkArea = () => {
     let target = document.querySelector("#setup");
     let workArea = document.createElement("div");
     workArea.setAttribute("id", "workArea");
-    let text = document.createElement("span");
+    let text = document.createElement("p");
+    text.classList.add("workAreaCurrent");
     text.innerHTML = "";
     workArea.appendChild(text);
     target.appendChild(workArea);
+}
+
+const typing = () => {
+
     document.addEventListener('keydown', (e) => {
+        let current = document.querySelector(".workAreaCurrent");
         switch (e.key) {
             case "Enter":
-                text.innerHTML += "<br/>";
+                let text = document.createElement("p");
+                text.classList.add("workAreaCurrent");
+                text.innerHTML = "";
+                current.classList.remove("workAreaCurrent");
+                current.parentElement.appendChild(text);
                 break;
             case "Backspace":
-                text.innerHTML = text.innerHTML.slice(0, -1);
+                current.innerHTML = current.innerHTML.slice(0, -1);
                 break;
             case "Meta":
             case "Control":
             case "Shift":
                 break;
             default:
-                text.innerHTML += e.key;
+                current.innerHTML += e.key;
                 break;
         }
 
