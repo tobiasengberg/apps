27d761342e3ebf254020c5242286e78ed1542cb9
Tobias Engberg <tobias.engberg@exilic.se>
2025-12-05 14:02:19 +0100

    Add backend template project

---
+++ react/cart/cart-backend/cart-backend/Program.cs
@@ -0,0 +1,14 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
