a4c531af5d0481566484947fe263db616b52e4b1
tobiasengberg <tobias.engberg@exilic.se>
2025-12-18 00:31:45 +0100

    Model further the workArea click event listener

--- vanilla-js/spreadsheet/events/eventlisteners.js
+++ vanilla-js/spreadsheet/events/eventlisteners.js
@@ -45,70 +45,90 @@ export const loadEventListeners = () => {
     })
 
     document.getElementById("workArea").addEventListener("click", (e) => {
-        eventListeners.columnsBar = true;
+        eventListeners.workArea = true;
         if(e.target.id === "workArea") return;
         if(!eventListeners.keyDownOnSelect) {
             eventListeners.keyDownOnSelect = true;
             document.addEventListener("keydown", handleKeyDown);
         }
-
         if(e.getModifierState("Meta")){
-            let added = addOrRemoveSelected(e.target.id);
-            added ? setSelectRectangle(e) : removeSelectRectangle();
-            if(config.selection.length === 0) {
-                document.removeEventListener("keydown", handleKeyDown);
-                eventListeners.keyDownOnSelect = false;
-            }
+            handleSelectedRange(e);
         } else if (config.selection.length === 1 && document.querySelector(".selected").id === e.target.id) {
-            const updateCellValue = (e) => {
-                if(e.target.value === "") {
-                    config.content = [...config.content.filter((item) => item.id !== newKey)];
-                    target.removeChild(target.firstChild);
-                } else {
-                    let item = config.content.filter((item) => item.id === newKey);
-                    if(item.length === 0) {
-                        config.content.push({
-                            value: e.target.value,
-                            id: newKey,
-                            column: parseInt(newKey.split("-")[1]),
-                            row: parseInt(newKey.split("-")[0]),
-                            style: []
-                        });
-                    } else {
-                        item[0].value = e.target.value;
-                    }
-                    config.selection.length = 0;
-                    eventListeners.blurEvent = false;
-                    reRender(1);
-                }
-            }
-
-            let target = document.getElementById(e.target.id);
-            let newKey = e.target.id;
-            let isContent = config.content.filter(item => item.id === e.target.id);
-            target.innerHTML = `<input id="cell-input" type="text" value=${isContent.length > 0 ? isContent[0].value : target.innerText}>`;
-            target.firstChild.focus();
-            target.firstChild.setSelectionRange(isContent[0].value.length, isContent[0].value.length)
-            if(!eventListeners.blurEvent) {
-                eventListeners.blurEvent = true;
-                target.firstChild.addEventListener("blur", updateCellValue);
-            }
-            target.firstChild.addEventListener("keydown", (e) => {
-                console.log(e.key === "=" && e.target.value.length === 0);
-                if(e.key === "Enter") {
-                    target.firstChild.blur();
-                }
-            })
-            target.firstChild.addEventListener("blur", updateCellValue);
+            handleDoubleClick(e);
         }
         else {
-            removePreviouslySelected();
-            addSingleSelected(e.target.id);
-            setSelectRectangle(e);
+            handleNewSelected(e);
         }
         if(!eventListeners.selectRectangle) {
             eventListeners.selectRectangle = true;
             document.getElementById("selectRectangle").addEventListener("mousedown", handleRectangleChange);
         }
     })
+}
+
+const handleSelectedRange = (e) => {
+    let added = addOrRemoveSelected(e.target.id);
+    added ? setSelectRectangle(e) : removeSelectRectangle();
+    if(config.selection.length === 0) {
+        document.removeEventListener("keydown", handleKeyDown);
+        eventListeners.keyDownOnSelect = false;
+    }
+}
+
+const handleDoubleClick = (e) => {
+    const updateCellValue = (e) => {
+        console.log(e.target.value);
+        if(e.target.value === "") {
+            config.content = [...config.content.filter((item) => item.id !== newKey)];
+        } else {
+            let item = config.content.filter((item) => item.id === newKey);
+            if(item.length === 0) {
+                config.content.push({
+                    value: e.target.value,
+                    id: newKey,
+                    column: parseInt(newKey.split("-")[1]),
+                    row: parseInt(newKey.split("-")[0]),
+                    style: []
+                });
+            } else {
+                item[0].value = e.target.value;
+            }
+        }
+        target.firstChild.removeEventListener("blur", updateCellValue);
+        target.firstChild.removeEventListener("keydown", checkKeydown);
+        target.removeChild(target.firstChild);
+        config.selection.length = 0;
+        eventListeners.focusKeydown = false;
+        eventListeners.blurEvent = false;
+        reRender(1);
+    }
+
+    const checkKeydown = (e) => {
+        console.log(e.key === "=" && e.target.value.length === 1);
+        if(e.key === "Enter") {
+            target.firstChild.blur();
+        }
+    }
+
+    let target = document.getElementById(e.target.id);
+    let newKey = e.target.id;
+    let isContent = config.content.filter(item => item.id === e.target.id);
+    console.log(isContent);
+    target.innerHTML = `<input id="cell-input" type="text" value=${isContent.length > 0 ? isContent[0].value : target.innerText}>`;
+    target.firstChild.focus();
+    if(isContent.length > 0) target.firstChild.setSelectionRange(isContent[0].value.length, isContent[0].value.length)
+    if(!eventListeners.blurEvent) {
+        eventListeners.blurEvent = true;
+        target.firstChild.addEventListener("blur", updateCellValue);
+    }
+    if(!eventListeners.focusKeydown) {
+        eventListeners.focusKeydown = true;
+        target.firstChild.addEventListener("keydown", checkKeydown);
+    }
+}
+
+const handleNewSelected = (e) => {
+    removePreviouslySelected();
+    addSingleSelected(e.target.id);
+    setSelectRectangle(e);
 }
